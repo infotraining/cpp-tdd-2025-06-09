8d57ca09843aeb793edad7f40fdc1ad4c3e54eb5
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-06-09 14:55:09 +0200

    Catch2 demo

--- catch-demo/tests/source_tests.cpp
+++ catch-demo/tests/source_tests.cpp
@@ -1,46 +1,163 @@
-#include <catch2/catch_test_macros.hpp>
 #include "source.hpp"
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+#include <catch2/catch_template_test_macros.hpp>
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <deque>
+#include <list>
 
 using namespace std;
 
-TEST_CASE("pushing items to vector", "[vector][push]")
+struct Value
 {
-    vector<int> vec{ 3 };
+    int value;
 
-    CHECK(vec.size() == 1);
-    CHECK(vec.capacity() == 1);
+    bool operator==(const Value& other) const = default;
+
+    friend std::ostream& operator<<(std::ostream& out, const Value& v)
+    {
+        out << "Value{" << v.value << "}";
+        return out;
+    }
+};
+
+Value operator+(const Value& v1, const Value& v2)
+{
+    return Value{v1.value + v2.value};
+}
+
+TEST_CASE("adding two numbers - operator+", "[math][operators][Value]")
+{
+    Value v1{42};
+    Value v2{2};
+
+    REQUIRE(v1 + v2 == Value{44});
+}
+
+TEST_CASE("Pushing items to vector")
+{
+    std::vector<int> vec = {1, 2, 3, 4};
 
     SECTION("push_back")
-    {       
-        vec.push_back(1);
+    {
+        vec.push_back(42);
 
         SECTION("size is increased")
         {
-            REQUIRE(vec.size() == 2);
+            REQUIRE(vec.size() == 5);
         }
 
-        SECTION("capacity can be increased")
+        SECTION("capacity is increased")
         {
-            REQUIRE(vec.capacity() >= 2);
+            REQUIRE(vec.capacity() >= 5);
+        }
+
+        SECTION("item is inserted at back")
+        {
+            REQUIRE(vec.back() == 42);
         }
     }
 }
 
-SCENARIO("pushing items to vector")
+SCENARIO("Pushing items to vector - G/W/T")
 {
-    GIVEN("vector with data")
+    GIVEN("vector with items")
     {
-        vector<int> vec = { 1, 2, 3 };
-        auto prev_size = vec.size();
+        std::vector<int> vec = {1, 2, 3, 4};
 
-        WHEN("push_back() an item")
+        WHEN("push_back")
         {
-            vec.push_back(4);
+            vec.push_back(42);
 
             THEN("size is increased")
             {
-                REQUIRE(vec.size() - prev_size == 1);
+                REQUIRE(vec.size() == 5);
+            }
+
+            AND_THEN("capacity is increased")
+            {
+                REQUIRE(vec.capacity() >= 5);
+            }
+
+            AND_THEN("item is inserted at back")
+            {
+                REQUIRE(vec.back() == 42);
             }
         }
     }
+}
+
+/////////////////////////////////////////
+// Parametrized tests
+
+TEST_CASE("adding two numbers", "[math][operators][Value]")
+{
+    const auto [a, b, expected] = GENERATE(
+        table<int, int, int>(
+            {
+                {1, 2, 3},
+                {42, 2, 44},
+                {53, -1, 52}
+            }
+        )
+    );
+
+    DYNAMIC_SECTION("" << a << " + " << b << " = " << expected)
+    {
+        Value v1{a};
+        Value v2{b};
+
+        REQUIRE(v1 + v2 == Value{expected});
+    }
+}
+
+TEMPLATE_TEST_CASE("vector can be resized", "[template]", int, std::string, float)
+{
+    std::vector<TestType> vec; // This setup will be done 3 times in total, once for each type
+    
+    CHECK(vec.size() == 0);
+    vec.resize(10);
+    REQUIRE(vec.size() == 10);
+}
+
+TEMPLATE_TEST_CASE("pushing items to container", "[template]", (std::vector<int>), (std::deque<int>), (std::list<int>) )
+{
+    using Container = TestType;
+    Container vec; // This setup will be done 3 times in total, once for each type
+    
+    auto pusher = [&vec]() { 
+        for(int i = 0; i < 1'000; ++i)
+            vec.push_back(i);
+        return vec.size();
+    };
+
+    BENCHMARK("pushing")
+    {
+        return pusher();                
+    };
+}
+
+int fibonacci(int n)
+{
+    if (n == 0)
+        return  0;
+    
+    if (n == 1)
+        return 1;
+    
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+TEST_CASE("fibonacci")
+{
+    BENCHMARK("fibonacci(4)")
+    {
+        return fibonacci(4);
+    };
+
+    BENCHMARK("fibonacci(20)")
+    {
+        return fibonacci(20);
+    };
 }
