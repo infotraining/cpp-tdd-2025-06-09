ab565afd1ffd0f48ec614bbec30ca3136ae860cf
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-06-09 11:33:30 +0200

    BowlingGame - Strike

--- tdd-katas/gtest/bowling/tests/bowling_tests.cpp
+++ tdd-katas/gtest/bowling/tests/bowling_tests.cpp
@@ -14,18 +14,21 @@ class BowlingGame
 {
     static constexpr uint32_t MAX_PINS_IN_FRAME = 10;
     static constexpr size_t MAX_ROLLS_IN_GAME = 20;
+
 public:
     constexpr BowlingGame() noexcept { }
 
     [[nodiscard]] uint32_t score() const noexcept
     {
         uint32_t result = 0;
-        for(size_t roll_index = 0; roll_index < pins_.size(); roll_index+=2)
+        for (size_t roll_index = 0; roll_index < pins_.size(); roll_index += 2)
         {
             result += frame_score(roll_index);
-            
-            if (is_spare(roll_index)) 
-                result += spare_bonus(roll_index);        
+
+            if(is_strike(roll_index))
+                result += strike_bonus(roll_index);
+            else if (is_spare(roll_index))
+                result += spare_bonus(roll_index);
         }
 
         return result;
@@ -34,7 +37,11 @@ public:
     void roll(uint32_t pins) noexcept
     {
         pins_[roll_index_] = pins;
-        ++roll_index_;        
+
+        if (is_strike(roll_index_))
+            ++roll_index_;
+
+        ++roll_index_;
     }
 
 private:
@@ -43,17 +50,27 @@ private:
 
     bool is_spare(size_t roll_index) const
     {
-        return pins_[roll_index] + pins_[roll_index+1] == MAX_PINS_IN_FRAME;
+        return pins_[roll_index] + pins_[roll_index + 1] == MAX_PINS_IN_FRAME;
+    }
+
+    bool is_strike(size_t roll_index) const
+    {
+        return roll_index_ % 2 == 0 && pins_[roll_index] == MAX_PINS_IN_FRAME;
     }
 
     uint32_t spare_bonus(size_t roll_index) const
     {
-        return pins_[roll_index+2];
+        return pins_[roll_index + 2];
+    }
+
+    uint32_t strike_bonus(size_t roll_index) const
+    {
+        return pins_[roll_index + 2] + pins_[roll_index + 3];
     }
 
     uint32_t frame_score(size_t roll_index) const
     {
-        return pins_[roll_index] + pins_[roll_index+1];
+        return pins_[roll_index] + pins_[roll_index + 1];
     }
 };
 
@@ -64,7 +81,7 @@ TEST(BowlingGameTests, WhenGameStarts_ThenScoreIsZero)
     ASSERT_EQ(game.score(), 0);
 }
 
-struct BowlingGame_SimpleRolls : ::testing::Test
+struct BowlingGameFixture : ::testing::Test
 {
     const int rolls_in_game = 20;
     BowlingGame game;
@@ -76,33 +93,51 @@ struct BowlingGame_SimpleRolls : ::testing::Test
     }
 };
 
-TEST_F(BowlingGame_SimpleRolls, WhenAllRollsInGutter_ThenScoreIsZero)
+struct BowlingGame_NoEvents : BowlingGameFixture
+{};
+
+TEST_F(BowlingGame_NoEvents, WhenAllRollsInGutter_ThenScoreIsZero)
 {
     roll_many(rolls_in_game, 0);
 
     ASSERT_EQ(game.score(), 0);
 }
 
-TEST_F(BowlingGame_SimpleRolls, WhenAllRollsWithNoBonus_ThenScoreIsSumOfPins)
+TEST_F(BowlingGame_NoEvents, WhenAllRollsWithNoBonus_ThenScoreIsSumOfPins)
 {
     roll_many(rolls_in_game, 1);
 
     ASSERT_EQ(game.score(), 20);
 }
 
-struct BowlingGame_WithEvents : BowlingGame_SimpleRolls
+struct BowlingGame_WithEvents : BowlingGameFixture
 {
     void roll_spare()
     {
         game.roll(1);
-        game.roll(9); 
+        game.roll(9);
+    }
+
+    void roll_strike()
+    {
+        game.roll(10);
     }
 };
 
-TEST_F(BowlingGame_WithEvents, WhenSpare_NextIsRollIsCountedTwice)
+TEST_F(BowlingGame_WithEvents, WhenSpare_NextRollIsCountedTwice)
 {
     roll_spare();
     roll_many(18, 1);
 
     ASSERT_EQ(game.score(), 29);
+}
+
+TEST_F(BowlingGame_WithEvents, WhenStrike_TwoNextRollsAreCountedTwice)
+{
+    roll_strike();
+    game.roll(6);
+    game.roll(2);
+    roll_many(16, 1);
+
+    ASSERT_EQ(game.score(), 42);
 }
