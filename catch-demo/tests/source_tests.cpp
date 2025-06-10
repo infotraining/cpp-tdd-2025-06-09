faa16d0cfd6a11fe0cf15f793b19ab3ee2acf583
Krystian Piękoś <krystian.piekos@infotraining.pl>
2025-06-10 20:12:44 +0200

    Refactor test cases for clarity and consistency; rearranged includes and added comments for Arrange, Act, Assert structure.

--- catch-demo/tests/source_tests.cpp
+++ catch-demo/tests/source_tests.cpp
@@ -1,11 +1,12 @@
 #include "source.hpp"
 
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_template_test_macros.hpp>
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/generators/catch_generators_all.hpp>
-#include <catch2/catch_template_test_macros.hpp>
-#include <catch2/benchmark/catch_benchmark.hpp>
 #include <deque>
 #include <list>
+#include <set>
 
 using namespace std;
 
@@ -37,25 +38,25 @@ TEST_CASE("adding two numbers - operator+", "[math][operators][Value]")
 
 TEST_CASE("Pushing items to vector")
 {
-    std::vector<int> vec = {1, 2, 3, 4};
+    std::vector<int> vec = {1, 2, 3, 4};   // Arrange
 
     SECTION("push_back")
     {
-        vec.push_back(42);
+        vec.push_back(42);  // Act
 
         SECTION("size is increased")
         {
-            REQUIRE(vec.size() == 5);
+            REQUIRE(vec.size() == 5);      // Assert
         }
 
         SECTION("capacity is increased")
         {
-            REQUIRE(vec.capacity() >= 5);
+            REQUIRE(vec.capacity() >= 5);  // Assert
         }
 
         SECTION("item is inserted at back")
         {
-            REQUIRE(vec.back() == 42);
+            REQUIRE(vec.back() == 42);    // Assert
         }
     }
 }
@@ -93,14 +94,20 @@ SCENARIO("Pushing items to vector - G/W/T")
 
 TEST_CASE("adding two numbers", "[math][operators][Value]")
 {
+    // const auto [a, b, expected] = GENERATE(
+    //     table<int, int, int>(
+    //         {
+    //             {1, 2, 3},
+    //             {42, 2, 44},
+    //             {53, -1, 52}
+    //         }
+    //     )
+    // );
+
     const auto [a, b, expected] = GENERATE(
-        table<int, int, int>(
-            {
-                {1, 2, 3},
-                {42, 2, 44},
-                {53, -1, 52}
-            }
-        )
+        std::tuple{1, 2, 3},
+        std::tuple{42, 2, 44},
+        std::tuple{53, -1, 52}
     );
 
     DYNAMIC_SECTION("" << a << " + " << b << " = " << expected)
@@ -115,38 +122,39 @@ TEST_CASE("adding two numbers", "[math][operators][Value]")
 TEMPLATE_TEST_CASE("vector can be resized", "[template]", int, std::string, float)
 {
     std::vector<TestType> vec; // This setup will be done 3 times in total, once for each type
-    
+
     CHECK(vec.size() == 0);
     vec.resize(10);
     REQUIRE(vec.size() == 10);
 }
 
-TEMPLATE_TEST_CASE("pushing items to container", "[template]", (std::vector<int>), (std::deque<int>), (std::list<int>) )
+TEMPLATE_TEST_CASE("pushing items to container", "[template]", (std::vector<int>), (std::deque<int>), (std::list<int>))
 {
     using Container = TestType;
     Container vec; // This setup will be done 3 times in total, once for each type
-    
-    auto pusher = [&vec]() { 
-        for(int i = 0; i < 1'000; ++i)
+
+    auto pusher = [&vec]()
+    {
+        for (int i = 0; i < 1'000; ++i)
             vec.push_back(i);
         return vec.size();
     };
 
     BENCHMARK("pushing")
     {
-        return pusher();                
+        return pusher();
     };
 }
 
 int fibonacci(int n)
 {
     if (n == 0)
-        return  0;
-    
+        return 0;
+
     if (n == 1)
         return 1;
-    
-    return fibonacci(n-1) + fibonacci(n-2);
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 TEST_CASE("fibonacci")
